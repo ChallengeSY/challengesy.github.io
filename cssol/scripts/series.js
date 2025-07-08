f32deefe7112fca1d26c43a54f76d4c91fe1b77e
Paul Ruediger <ChallengeSY@users.noreply.github.com>
2025-07-08 19:03:30 -0400

    Update series.js
    Hotfix out the extraneous char.

--- cssol/scripts/series.js
+++ cssol/scripts/series.js
@@ -2,7 +2,7 @@ baseStatFile = "seriesPlay";
 window.onbeforeunload = function(event) {confirmLeave(event, "Leaving this game will save your series, but reset your current attempt to the beginning.")};
 
 function finishSeriesGame() {
-	var nextGame = (!solGame.recordWin || !solGame.gameActive0);
+	var nextGame = (!solGame.recordWin || !solGame.gameActive);
 	var redoGame = false;
 	var endSeries = (seriesGame > 3 || seriesLives < 1);
 	
